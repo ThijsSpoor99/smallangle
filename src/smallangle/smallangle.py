07181c37f6152717fbd647fa530e2a1709db8183
ThijsSpoor99 <tspoor@live.nl>
2023-11-17 14:34:03 +0100

    Created sin and tan command

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -3,20 +3,33 @@ import numpy as np
 from numpy import pi
 import pandas as pd
 
+@click.group()
+def smallangle():
+    pass
 
+@smallangle.command()
+@click.option(
+    '-n',
+    '--number',
+    default=10
+)
 def sin(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
     return
 
-
+@smallangle.command()
+@click.option(
+    '-n',
+    '--number',
+    default=10
+)
 def tan(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
     return
 
-
 if __name__ == "__main__":
-    sin(10)
+    smallangle()
