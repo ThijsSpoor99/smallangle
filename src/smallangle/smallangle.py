7b8122dfdddfce1d1c71249741f1899de17ae58a
ThijsSpoor99 <tspoor@live.nl>
2023-11-17 15:31:39 +0100

    Added docstring

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -11,9 +11,15 @@ def smallangle():
 @click.option(
     '-n',
     '--number',
-    default=10
+    default=10,
+    help='number of steps between 0 and 2pi'
 )
 def sin(number):
+    """shows sin(x) from 0 to 2pi
+
+    Args:
+        number (number): number of steps between 0 and 2pi
+    """
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
@@ -23,9 +29,15 @@ def sin(number):
 @click.option(
     '-n',
     '--number',
-    default=10
+    default=10,
+    help='number of steps between 0 and 2pi'
 )
 def tan(number):
+    """shows tan(x) from 0 to 2pi
+
+    Args:
+        number (int): number of steps between 0 and 2pi
+    """
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
